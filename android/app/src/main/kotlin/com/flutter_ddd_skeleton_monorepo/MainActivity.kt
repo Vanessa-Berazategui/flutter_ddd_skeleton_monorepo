f7e685e590d2db5285f1d33a9b20ed8999d30aaf
Vanessa Berazategui <vberazategui@geocom.com.uy>
2024-06-25 15:56:01 -0300

    ref: android build

---
+++ android/app/src/main/kotlin/com/flutter_ddd_skeleton_monorepo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flutter_ddd_skeleton_monorepo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/vberazateguisilva/flutter_ddd_skeleton_monorepo_flavour_1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.vberazateguisilva.flutter_ddd_skeleton_monorepo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
