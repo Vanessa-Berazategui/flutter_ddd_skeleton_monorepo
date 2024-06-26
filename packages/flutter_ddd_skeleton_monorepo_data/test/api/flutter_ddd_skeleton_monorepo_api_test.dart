211f468232a8cef3f1a8fbc14755b2aa23b705d5
Vanessa Berazategui <vberazategui@geocom.com.uy>
2024-06-26 14:35:15 -0300

    chore: add helpers files

--- packages/flutter_ddd_skeleton_monorepo_data/test/api/flutter_ddd_skeleton_monorepo_api_test.dart
+++ packages/flutter_ddd_skeleton_monorepo_data/test/api/flutter_ddd_skeleton_monorepo_api_test.dart
@@ -5,23 +5,7 @@ import 'package:flutter_ddd_skeleton_monorepo_data/flutter_ddd_skeleton_monorepo
 // Project imports:
 import 'package:flutter_test/flutter_test.dart';
 
-class AppEnvironmentApi {
-  const AppEnvironmentApi();
-
-  String get baseUrl => 'https://numbersapi.p.rapidapi.com';
-
-  String get apiKey => '3954d4f3abmsh5ca86014f157f79p1f5bd4jsn90e988834ad6';
-
-  String get apiHost => 'numbersapi.p.rapidapi.com';
-
-  int get connectTimeout => 30000;
-
-  int get receiveTimeout => 50000;
-
-  int get sendTimeout => 30000;
-
-  String get env => 'api';
-}
+import '../helpers/helpers.dart';
 
 void main() {
   /// Api
---
+++ packages/flutter_ddd_skeleton_monorepo_data/test/helpers/app_environment_api.dart
@@ -0,0 +1,17 @@
+class AppEnvironmentApi {
+  const AppEnvironmentApi();
+
+  String get baseUrl => 'https://numbersapi.p.rapidapi.com';
+
+  String get apiKey => '3954d4f3abmsh5ca86014f157f79p1f5bd4jsn90e988834ad6';
+
+  String get apiHost => 'numbersapi.p.rapidapi.com';
+
+  int get connectTimeout => 30000;
+
+  int get receiveTimeout => 50000;
+
+  int get sendTimeout => 30000;
+
+  String get env => 'api';
+}
