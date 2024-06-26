176ccdc7e210640190cd9a4e8cbf3daf8dade380
Vanessa Berazategui <vberazategui@geocom.com.uy>
2024-06-26 11:05:29 -0300

    chore: add integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -3,49 +3,38 @@
 // Project imports:
 import 'package:flutter_ddd_skeleton_monorepo/app/app.dart';
 import 'package:flutter_ddd_skeleton_monorepo/counter/counter.dart';
-import 'package:flutter_ddd_skeleton_monorepo/main_production.dart' as app;
+import 'package:flutter_ddd_skeleton_monorepo/main_development.dart' as app;
 // Package imports:
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
   group('App', () {
-    testWidgets('renders App', (tester) async {
+    IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+    testWidgets('Integration App Test', (tester) async {
+      // Load app
       app.main();
       await tester.pumpAndSettle();
       expect(find.byType(CounterView), findsOneWidget);
-    });
-  });
-
-  group('CounterView', () {
-    testWidgets('renders current value', (tester) async {
-      await tester.pumpWidget(const App());
       expect(find.text('0'), findsOneWidget);
-    });
 
-    testWidgets('see value of 1 when increment button is tapped',
-        (tester) async {
-      await tester.pumpWidget(const App());
+      // Tap increment
       await tester.tap(find.byIcon(Icons.add));
+      await Future<void>.delayed(const Duration(seconds: 2));
       await tester.pumpAndSettle();
       expect(find.text('1'), findsOneWidget);
-    });
 
-    testWidgets('see value of -1 when increment button is tapped',
-        (tester) async {
-      await tester.pumpWidget(const App());
+      // Tap decrement
       await tester.tap(find.byIcon(Icons.remove));
+      await Future<void>.delayed(const Duration(seconds: 2));
       await tester.pumpAndSettle();
-      expect(find.text('-1'), findsOneWidget);
-    });
+      expect(find.text('0'), findsOneWidget);
 
-    testWidgets('see quiz when quiz button is tapped', (tester) async {
-      await tester.pumpWidget(const App());
-      await tester.tap(find.byIcon(Icons.add));
-      await tester.pumpAndSettle();
+      // Tap quiz
       await tester.tap(find.byIcon(Icons.quiz));
+      await Future<void>.delayed(const Duration(seconds: 2));
+      await tester.pumpAndSettle();
       expect(find.byKey(const Key('quiz')), findsOneWidget);
     });
   });
