7aa838cc997c3660efe15d047dd9dfbf09722c07
Vanessa Berazategui <vberazategui@geocom.com.uy>
2024-06-27 10:59:57 -0300

    chore: fix doc

--- test/helpers/app_environment_testing.dart
+++ test/helpers/app_environment_testing.dart
@@ -11,7 +11,7 @@ class AppEnvironmentTesting implements AppEnvironment {
   String get baseUrl => 'https://numbersapi.p.rapidapi.com';
 
   @override
-  String get apiKey => '3954d4f3abmsh5ca86014f157f79p1f5bd4jsn90e988834ad6';
+  String get apiKey => '';
 
   @override
   String get apiHost => 'numbersapi.p.rapidapi.com';
