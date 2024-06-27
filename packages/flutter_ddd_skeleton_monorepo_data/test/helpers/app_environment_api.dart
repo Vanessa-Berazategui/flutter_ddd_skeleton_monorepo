f582316324218fb05c55c6d5cdd60a8ffc7db416
Vanessa Berazategui <vberazategui@geocom.com.uy>
2024-06-27 10:43:52 -0300

    chore: fixing doc

--- packages/flutter_ddd_skeleton_monorepo_data/test/helpers/app_environment_api.dart
+++ packages/flutter_ddd_skeleton_monorepo_data/test/helpers/app_environment_api.dart
@@ -3,7 +3,7 @@ class AppEnvironmentApi {
 
   String get baseUrl => 'https://numbersapi.p.rapidapi.com';
 
-  String get apiKey => '3954d4f3abmsh5ca86014f157f79p1f5bd4jsn90e988834ad6';
+  String get apiKey => '';
 
   String get apiHost => 'numbersapi.p.rapidapi.com';
 
